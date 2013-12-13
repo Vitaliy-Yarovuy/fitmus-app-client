3ccad0666d15c488b9366ed5a28d9a4e898df27b
vitaliy <vitaliy.yxz@gmail.com>
2013-12-13 22:39:08 +0200

    update build

--- www/js/utils/files.utils.js
+++ www/js/utils/files.utils.js
@@ -1,12 +1,16 @@
 (function(global){
+    var LocalFileSystem = function() {
+    };
+    LocalFileSystem.TEMPORARY = 0; //temporary, with no guarantee of persistence
+    LocalFileSystem.PERSISTENT = 1; //persistent
 
     var fUtils = {
         getContentDirectory: function(cb){
             window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function(fileSystem){
-                alert(fileSystem.name);
-                alert(fileSystem.root.name);
+                console.log(fileSystem.name);
+                console.log(fileSystem.root.name);
             }, function(event){
-                alert(event.target.error.code);
+                console.log(event.target.error.code);
             });
         }
     };
