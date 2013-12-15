5b220d8f0405bfec8dec50de3d54e6dc6f015b77
vitaliy <vitaliy.yxz@gmail.com>
2013-12-15 13:48:54 +0200

    update build

--- js/directives/exerciseImg.js
+++ js/directives/exerciseImg.js
@@ -7,11 +7,13 @@ app.directive('ngExerciseImg', function($compile, $rootScope, connect) {
                 exercise = scope.$eval(attrs.ngExerciseImg) || fakeExercise,
                 src = exercisesUri[exercise.id] || exercise.img;
             $element.attr("src",src);
-            $element.addAfter("")
+            var $src = $element.after('<span style="position:absolute;"></span>');
+            $src.html(src);
             scope.$watch(attrs.ngExerciseImg, function(exercise){
                 exercise = exercise || fakeExercise;
                 var src = exercisesUri[exercise.id] || exercise.img;
                 $element.attr("src",src);
+                $src.html(src);
             })
         }
     };
