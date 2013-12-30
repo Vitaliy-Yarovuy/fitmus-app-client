6db02e23040f42204017e1287ff9a0dd0a93e629
vitaliy <vitaliy.yxz@gmail.com>
2013-12-30 20:45:44 +0200

    update build

---
+++ js/services/message.js
@@ -0,0 +1,32 @@
+'use strict';
+
+app.factory('message',function ($rootScope){
+    return {
+        /**
+         * alert message
+         * @param message
+         * @param callback
+         */
+        alert: function(message, callback){
+            navigator.notification.alert(
+                message,                   // message
+                callback || function(){},  // callback
+                'Внимание',                // title
+                'хорошо'                   // buttonName
+            );
+        },
+        /**
+         * confirm action
+         * @param message
+         * @param callback
+         */
+        confirm: function(message, callback){
+            navigator.notification.alert(
+                message,                  // message
+                callback || function(){}, // callback to invoke with index of button pressed
+                'Подтверждение',          // title
+                'Применить,Отменить'      // buttonLabels
+            );
+        }
+    };
+});
